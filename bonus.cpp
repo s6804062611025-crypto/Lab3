2b5f3bfe9ffaa389ca9c41c77942ef0359f00274
s6804062611025-crypto <s6804062611025@email.kmutnb.ac.th>
2025-12-15 09:59:42 +0700

    Add files via upload

---
+++ bonus.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+	{	
+		int score;
+		scanf("%d",&score);
+		if (score >= 68)
+			if (score > 100)
+				printf("error score");
+			else if (score >= 85)
+				printf("A");
+			else if (score >= 75)
+				printf("B");
+			else printf("C %d",79-score);
+		else if (score >= 55)
+			printf("D");
+		else printf("F");
+
+		return 0;
+	}
